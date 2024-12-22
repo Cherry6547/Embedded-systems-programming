d38921594429192edf370f4dd560824c0ec7ac77
Cherry6547 <charansaimathkala@gmail.com>
2024-12-22 16:20:41 +0530

    Added comments for better understanding of code

--- Controlling_ledsUsing_Switch.ino
+++ Controlling_ledsUsing_Switch.ino
@@ -2,20 +2,20 @@ void setup() {
   // put your setup code here, to run once:
   volatile char *dir,*in1,*out1,count=0;
   volatile long i;
-  dir = 0x30;
-  *dir = 0x01;
-  out1 = 0x31;
-  dir = 0x107;
-  *dir = 0x00;
-  in1 = 0x106;
+  dir = 0x30;  // PORT F Direction Register 
+  *dir = 0x01; // PORT F PIN 0 BEING configured as OUTPUT
+  out1 = 0x31; // PORT F output 
+  dir = 0x107; //  PORT K Direction Register
+  *dir = 0x00; // PORT K PIN 0 BEING configured as INPUT
+  in1 = 0x106; // PORT K INPUT ADDRESS
   while(1){
-    if((*in1 & 0x01) == 0x01){
-      *out1 = 0x01;
+    if((*in1 & 0x01) == 0x01){ //masking is done to get proper input 
+      *out1 = 0x01; // if button is pushed then led will turn on
     }
     else{
-      *out1 = 0x00;
+      *out1 = 0x00; // if button is not pushed then led will remain off
     }
-    for(i=0;i<40000;i++);
+    for(i=0;i<40000;i++); // delay is maintained to see output clearly
   }
 
 }
